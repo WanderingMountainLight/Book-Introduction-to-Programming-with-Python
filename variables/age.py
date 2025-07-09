359792842bf6cc61c4cbf4d7344fbb013ddfac0a
Steve Milanes <stevemilanes@icloud.com>
2025-07-09 14:29:57 -0400

    Adding Variables exercises Updating Author info

---
+++ variables/age.py
@@ -0,0 +1,12 @@
+#Variable Exercise 6
+
+#Write a program named age.py that includes someone's age and then calculates and reports the future age 
+#10, 20, 30, and 40 years from now. Here's the output for someone who is 22 years old.
+
+age =  22  # Change this value to test with different ages.
+
+print("You are " + str(age) + " years old.")
+print("In 10 years you will be " + str(age + 10) + " years old.")
+print("In 20 years you will be " + str(age + 20) + " years old.")
+print("In 30 years you will be " + str(age + 30) + " years old.")
+print("In 40 years you will be " + str(age + 40) + " years old.")
